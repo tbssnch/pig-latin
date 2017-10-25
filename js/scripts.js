2aa5673338f8acbbc80db85f2987c94ed95fb8b6
Sam Kissee and Tyler Bussanich <sam@>
2017-10-25 15:21:16 -0700

    add functions to convert words to piglatin

--- js/scripts.js
+++ js/scripts.js
@@ -5,24 +5,47 @@ var toPigLatin = function(sentence) {
   var twoLetter = sentence.substring(0,2);
   var twoLetterLast = sentence.substring(2, sentence.length);
   var threeLetter = sentence.substring(0,3);
-  var firstLast = sentence.substring(3, sentence.length);
-  var vowels = ["a","e", "i", "o", "u", "A","E", "I", "O", "U"];
+  var threeLast = sentence.substring(3, sentence.length);
+  var vowels = ["a","e", "i", "o", "u", "A","E", "I", "O", "U", "y", "Y"];
+  var qu = "qu"
   for (var index = 0; index < firstLetter.length; index++){
-      for (var vowelIndex =0; vowelIndex < vowels.length; vowelIndex++) {
-        if (firstLetter[index] === vowels[vowelIndex]){
+      for (var vowelIndex =0; vowelIndex < vowels.length; vowelIndex++)
+      { if (firstLetter === "y"){
+        return firstLast + firstLetter + "ay"
+      } else if (firstLetter[index] === vowels[vowelIndex]){
          return sentence + "way";
-        };
-      };
+       }
+     }
+   };
+  for (var index = 0; index < twoLetter.length; index++){
+    for (var vowelIndex =0; vowelIndex < vowels.length; vowelIndex++) {
+      if (twoLetter === qu ){
+        return twoLetterLast + twoLetter + "ay"
+      } else if (twoLetter[index] === vowels[vowelIndex]){
+        return firstLast + firstLetter + "ay"
+      }
+      }
     };
-};
+  for (var index = 0; index < threeLetter.length; index++){
+    for (var vowelIndex =0; vowelIndex < vowels.length; vowelIndex++) {
+      if (threeLetter[index] === vowels[vowelIndex]){
+        return twoLetterLast + twoLetter + "ay"
+      }
+       else {
+       return sentence
+      }
+      }
+    };
+    };
 
 
 $(document).ready(function() {
   $("form#pig-form").submit(function(event) {
     event.preventDefault();
     var userInput = $("input#pigLatin").val();
-    var splt = userInput.split("");
-    var pigLatin = toPigLatin(userInput);
+    var stringInput = userInput.slice();
+    // var splt = userInput.split("");
+    var pigLatin = toPigLatin(userInput.slice());
 
     $("#result").append("<p>" + pigLatin + "</p>")
 
